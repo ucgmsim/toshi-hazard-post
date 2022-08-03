bb2b75d84e2ac10920795ab7965428a83aff5602
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-08-03 16:06:48 +1200

    bump version; fix API_KEY setup for tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Changelog
 
-## 0.1.0 (2022-07-20)
+## [0.2.0] - 2022-08-03
+
+### Added
+ * A working Dockerfile fpor batch
+ * main aggregation code ported from THS;
+ * dynamodb via THS
+ * runs hazard aggregration in AWS_BATCH mode
+
+## [0.1.0] - 2022-07-20
 
 * First release on PyPI.
--- toshi_hazard_post/local_config.py
+++ toshi_hazard_post/local_config.py
@@ -22,6 +22,8 @@ try:
         API_KEY = get_secret("NZSHM22_TOSHI_API_SECRET_TEST", "us-east-1").get("NZSHM22_TOSHI_API_KEY_TEST")
     elif 'PROD' in API_URL.upper():
         API_KEY = get_secret("NZSHM22_TOSHI_API_SECRET_PROD", "us-east-1").get("NZSHM22_TOSHI_API_KEY_PROD")
+    else:
+        API_KEY = os.getenv('NZSHM22_TOSHI_API_KEY', "")
 except AttributeError as err:
     print(f"unable to get secret from secretmanager: {err}")
     API_KEY = os.getenv('NZSHM22_TOSHI_API_KEY', "")
