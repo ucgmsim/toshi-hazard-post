143f140720e8377e5dcb8898108acba5c2c57680
chrisdicaprio <christopher.dicaprio@gmail.com>
2022-08-17 12:12:56 +1200

    single deagg realization

---
+++ toshi_hazard_post/hazard_aggregation/deaggregation.py
@@ -0,0 +1,305 @@
+import json
+import logging
+import math
+import multiprocessing
+import time
+from collections import namedtuple
+from typing import List
+
+import numpy as np
+from nzshm_common.location.code_location import CodedLocation
+from nzshm_common.location.location import LOCATIONS_BY_ID
+from toshi_hazard_store.query_v3 import get_hazard_curves
+
+from toshi_hazard_post.branch_combinator import merge_ltbs_fromLT
+from toshi_hazard_post.hazard_aggregation.aggregate_rlzs import compute_hazard_at_poe
+from toshi_hazard_post.hazard_aggregation.aggregation import build_source_branches
+from toshi_hazard_post.hazard_aggregation.locations import get_locations
+from toshi_hazard_post.local_config import NUM_WORKERS
+
+from .aggregate_rlzs import (
+    build_branches,
+    build_rlz_table,
+    calculate_aggs,
+    get_imts,
+    get_levels,
+    get_source_and_gsim,
+    load_realization_values,
+    prob_to_rate,
+    rate_to_prob,
+)
+from .aggregation_config import AggregationConfig
+
+log = logging.getLogger(__name__)
+
+DeAggTaskArgs = namedtuple(
+    "DeAggTaskArgs", "hazard_model_id grid_loc locs toshi_ids source_branches aggs imts levels vs30 poes inv_time"
+)
+
+
+def process_location_list_deagg(task_args):
+    """The math happens inside here... REFACTOR ME. ported from THS."""
+    NUM_RLZ = 100
+
+    hazard_model_id = task_args.hazard_model_id
+    locs = task_args.locs
+    toshi_ids = task_args.toshi_ids
+    source_branches = task_args.source_branches
+    aggs = task_args.aggs
+    imts = task_args.imts
+    levels = task_args.levels
+    vs30 = task_args.vs30
+    poes = task_args.poes
+    inv_time = task_args.inv_time
+
+    # print(locs)
+    log.info('get values for %s locations and %s hazard_solutions' % (len(locs), len(toshi_ids)))
+    log.debug('aggs: %s' % (aggs))
+    log.debug('imts: %s' % (imts))
+    log.debug('toshi_ids[:3]: %s' % (toshi_ids[:3]))
+
+    # log.debug('source_branches: %s' % (source_branches))
+
+    tic_fn = time.perf_counter()
+    values = load_realization_values(toshi_ids, locs, [vs30])
+
+    if not values:
+        log.info('missing values: %s' % (values))
+        return
+
+    k1 = next(iter(values.keys()))
+    k2 = next(iter(values[k1].keys()))
+    k3 = next(iter(values[k1][k2].keys()))
+    rate_shape = values[k1][k2][k3].shape
+
+    deagg_rlzs = []
+    for loc in locs:
+        for poe in poes:
+            for agg in aggs:
+                for imt in imts:
+                    # get target level of shaking
+                    hc = next(
+                        get_hazard_curves([loc], [vs30], [hazard_model_id], [imt], [agg])
+                    )  # TODO move up to reduce number of queries?
+
+                    levels = []
+                    hazard_vals = []
+                    for v in hc.values:
+                        levels.append(v.lvl)
+                        hazard_vals.append(v.val)
+
+                    target_level = compute_hazard_at_poe(levels, hazard_vals, poe, inv_time)
+                    min_dist = math.inf
+
+                    # find realization with nearest level of shaking
+                    # TODO: repeating a lot of code here. Unify with agg processing?
+                    nbranches = len(source_branches) * len(source_branches[0]['weight_combs'])
+                    log.info(f'nbranches: {nbranches}')
+                    # dists = np.empty((nbranches,))
+                    deagg_specs = []
+                    for branch in source_branches:
+                        rlz_combs = branch['rlz_combs']
+                        weight_combs = branch['weight_combs']
+                        branch_weight = branch['weight']
+
+                        for weight, rlz_comb in zip(weight_combs, rlz_combs):
+                            rate = np.zeros(rate_shape)
+                            for rlz in rlz_comb:
+                                rate += prob_to_rate(values[rlz][loc][imt])
+                            prob = rate_to_prob(rate)
+                            rlz_level = compute_hazard_at_poe(levels, prob, poe, inv_time)
+                            # dist = abs(rlz_level - target_level)
+                            if dist < min_dist:
+                                nearest_rlz = rlz_comb
+                                min_dist = dist
+                                nearest_level = rlz_level
+                                rlz_weight = branch_weight * weight
+                                rank = 0
+
+                    hazard_ids = [id.split(':')[0] for id in nearest_rlz]
+                    source_ids, gsims = get_source_and_gsim(nearest_rlz, vs30)
+
+                    deagg_spec = dict(
+                        level=nearest_level,
+                        source_ids=source_ids,
+                        gsims=gsims,
+                        nearest_rlz=nearest_rlz,
+                        hazard_ids=hazard_ids,
+                        weight=rlz_weight,
+                        rank=rank,
+                        dist=min_dist,
+                    )
+                    deagg_specs.append(deagg_spec)
+                    log.info(f'deagg_spec: {deagg_spec}')
+
+                    deagg_rlzs.append(
+                        dict(
+                            vs30=vs30,
+                            inv_time=inv_time,
+                            imt=imt,
+                            agg=agg,
+                            poe=poe,
+                            location=loc,
+                            target_level=target_level,
+                            deagg_specs=deagg_specs,
+                        )
+                    )
+
+    return deagg_rlzs
+
+
+class DeAggregationWorkerMP(multiprocessing.Process):
+    """A worker that batches and saves records to DynamoDB. ported from THS."""
+
+    def __init__(self, task_queue, result_queue):
+        multiprocessing.Process.__init__(self)
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+
+    def run(self):
+        log.info("worker %s running." % self.name)
+        proc_name = self.name
+
+        while True:
+            nt = self.task_queue.get()
+            if nt is None:
+                # Poison pill means shutdown
+                self.task_queue.task_done()
+                log.info('%s: Exiting' % proc_name)
+                break
+
+            disagg_configs = process_location_list_deagg(nt)
+
+            self.task_queue.task_done()
+
+            log.info('%s task done.' % self.name)
+            self.result_queue.put(disagg_configs)
+
+
+def process_local_deagg(hazard_model_id, toshi_ids, source_branches, coded_locations, levels, config, num_workers):
+    """Run task locally using Multiprocessing. ported from THS."""
+
+    task_queue: multiprocessing.JoinableQueue = multiprocessing.JoinableQueue()
+    result_queue: multiprocessing.Queue = multiprocessing.Queue()
+
+    num_workers = 1  # TODO: remove me!!!
+    print('Creating %d workers' % num_workers)
+    workers = [DeAggregationWorkerMP(task_queue, result_queue) for i in range(num_workers)]
+    for w in workers:
+        w.start()
+
+    tic = time.perf_counter()
+    # Enqueue jobs
+    num_jobs = 0
+
+    toshi_ids = {int(k): v for k, v in toshi_ids.items()}
+    source_branches = {int(k): v for k, v in source_branches.items()}
+
+    # log.info('cget values for %s locations and %s hazard_solutions' % (len(locs), len(toshi_ids)))
+    for coded_loc in coded_locations:
+        for vs30 in config.deagg_vs30s:
+            t = DeAggTaskArgs(
+                hazard_model_id,
+                coded_loc.downsample(0.1).code,
+                [coded_loc.downsample(0.001).code],
+                toshi_ids[vs30],
+                source_branches[vs30],
+                config.deagg_aggs,
+                config.deagg_imts,
+                levels,
+                vs30,
+                config.deagg_poes,
+                config.deagg_invtime,
+            )
+
+            task_queue.put(t)
+            num_jobs += 1
+
+    # Add a poison pill for each to signal we've done everything
+    for i in range(num_workers):
+        task_queue.put(None)
+
+    # Wait for all of the tasks to finish
+    task_queue.join()
+
+    results = []
+    while num_jobs:
+        result = result_queue.get()
+        results += result
+        num_jobs -= 1
+
+    toc = time.perf_counter()
+    print(f'time to run deaggregations: {toc-tic:.0f} seconds')
+    return results
+
+
+def add_site_name(disagg_configs):
+    resolution = 0.001
+    disagg_configs_copy = disagg_configs.copy()
+    for disagg_config in disagg_configs_copy:
+        for loc in LOCATIONS_BY_ID.values():
+            location = CodedLocation(loc['latitude'], loc['longitude'], resolution).downsample(0.001).code
+            if location == disagg_config['location']:
+                disagg_config['site_code'] = loc['id']
+                disagg_config['site_name'] = loc['name']
+
+    return disagg_configs_copy
+
+
+def process_deaggregation(config: AggregationConfig):
+
+    if not config.deaggregation:
+        raise Exception('a deaggregation configuration must be specified for deagg mode')
+    try:
+        config.validate_deagg()
+    except:
+        raise Exception('invalid deaggregation configuration')
+
+    # assume an aggregation has already been performed that covers the parameters requested in the deagg. This can waste time in 2 ways
+    # 1) must re-construct the source_branches
+    # 2) if an agg has not been performed then the query comes back empty and we've wasted our time
+    omit: List[str] = []
+
+    toshi_ids = {}
+    for vs30 in config.deagg_vs30s:
+        toshi_ids[vs30] = [
+            b.hazard_solution_id
+            for b in merge_ltbs_fromLT(config.logic_tree_permutations, gtdata=config.hazard_solutions, omit=omit)
+            if b.vs30 == vs30
+        ]
+
+    source_branches = {}
+    for vs30 in config.deagg_vs30s:
+        source_branches[vs30] = build_source_branches(
+            config.logic_tree_permutations,
+            config.hazard_solutions,
+            config.src_correlations,
+            config.gmm_correlations,
+            vs30,
+            omit,
+            truncate=config.source_branches_truncate,
+        )
+
+    # TODO deagg gets own location list?
+    locations = get_locations(config)
+
+    resolution = 0.001
+    coded_locations = [CodedLocation(*loc, resolution) for loc in locations]
+
+    example_loc_code = coded_locations[0].downsample(0.001).code
+
+    levels = get_levels(
+        source_branches[config.vs30s[0]], [example_loc_code], config.vs30s[0]
+    )  # TODO: get seperate levels for every IMT
+    avail_imts = get_imts(source_branches[config.vs30s[0]], config.vs30s[0])
+    for imt in config.imts:
+        assert imt in avail_imts
+
+    deagg_configs = process_local_deagg(
+        config.hazard_model_id, toshi_ids, source_branches, coded_locations, levels, config, NUM_WORKERS
+    )
+
+    deagg_configs = add_site_name(deagg_configs)
+
+    with open('deagg_configs.json', 'w') as deagg_file:
+        json.dump(deagg_configs, deagg_file)
