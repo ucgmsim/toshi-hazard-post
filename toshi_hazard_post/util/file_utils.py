a50d9184cc123b06ad50d3480675678d4997c849
chrisdicaprio <christopher.dicaprio@gmail.com>
2022-08-27 19:13:02 +1200

    disagg utils

---
+++ toshi_hazard_post/util/file_utils.py
@@ -0,0 +1,117 @@
+import time
+import io
+import csv
+import json
+import logging
+import itertools
+
+import pandas as pd
+import numpy as np
+
+from zipfile import ZipFile
+from collections import namedtuple
+
+from nzshm_common.location.code_location import CodedLocation
+
+log = logging.getLogger(__name__)
+
+
+def disagg_df(rlz_names):
+
+    columns = ['imt','mag','dist','trt'] + rlz_names
+    imts = ['PGA']
+    mags = np.arange(5.25,10,.5)
+    dists = np.arange(5,550,10)
+    trts =  ['Active Shallow Crust', 'Subduction Interface', 'Subduction Intraslab']
+    index = range(len(imts) * len(mags) * len(dists) * len(trts))
+    disaggs = pd.DataFrame(columns=columns, index=index)
+    for i, (imt, mag, dist, trt) in enumerate(itertools.product(imts, mags, dists, trts)):
+        disaggs.loc[i,'imt'] = imt
+        disaggs.loc[i,'mag'] = mag
+        disaggs.loc[i,'dist'] = dist
+        disaggs.loc[i,'trt'] = trt
+        for rlz in rlz_names:
+            disaggs.loc[i,rlz] = 0
+            
+    return disaggs
+
+
+
+def get_location(header):
+    """
+    get the location from the disagg csv
+    this is terrible and hacky and only temporory until disagg data is stored by THS
+    """
+
+    info = header[-1]
+    start_lon = info.index('lon=')+4
+    tail = info[start_lon:]
+    try:
+        end_lon = tail.index(',')
+        lon = tail[:end_lon]
+    except:
+        lon = tail[:]
+
+    start_lat = info.index('lat=')+4
+    tail = info[start_lat:]
+    try:
+        end_lat = tail.index(',')
+        lat = tail[:end_lat]
+    except:
+        lat = tail[:]
+
+    location = CodedLocation(float(lat),float(lon),0.001).code
+
+    return location
+
+def get_disagg_mdt(csv_archive):
+    """
+    get the disagg data from the csv archive
+    this is terrible and hacky and only temporory until disagg data is stored by THS
+    assuming only 1 location and 1 imt
+    """
+
+    # because each hazard_id only has a single TRT, we must insert zeros for the other TRTs to treat all disagg arrays the same
+    # assume the order and TRTs to be Active Shallow Crust, Subduction Interface, Subduction Intraslab
+    TRT_ORDER = ['Active Shallow Crust', 'Subduction Interface', 'Subduction Intraslab']
+    ntrt = len(TRT_ORDER)
+
+    with ZipFile(csv_archive) as zipf:
+        with io.TextIOWrapper(zipf.open('Mag_Dist_TRT-0_1.csv'), encoding="utf-8") as mag_dist_TRT_file:
+            disagg_reader = csv.reader(mag_dist_TRT_file)
+            header0 = next(disagg_reader)
+            location = get_location(header0)
+
+            header = next(disagg_reader)
+            DisaggData = namedtuple("DisaggData", header, rename=True)
+            rlz_names = header[5:]
+            disaggs = disagg_df(rlz_names)
+
+            for row in disagg_reader:
+                disagg_data = DisaggData(*row)
+                imt = disagg_data.imt
+                mag = float(disagg_data.mag)
+                dist = float(disagg_data.dist)
+                trt = disagg_data.trt
+                ind = (disaggs['imt'].isin([imt])) & (disaggs['mag'].isin([mag])) & (disaggs['dist'].isin([dist])) & (disaggs['trt'].isin([trt]))
+                disaggs.iloc[ind,4:] = list(map(float,row[5:]))
+                
+                # for rlz in rlz_names:
+                    # disaggs.loc[ind, rlz ] = float(getattr(disagg_data,rlz))
+
+    disaggs_dict = {}    
+    for rlz in rlz_names:
+        disaggs_dict[rlz[3:]] = disaggs[rlz].to_numpy(dtype='float64')
+
+    return disaggs_dict, location, imt
+
+
+def save_deaggs(deagg_data, loc, imt):
+
+    deagg_filename = f'deagg_{loc}_{imt}.npy'
+    # with open(deagg_filename,'w') as jsonfile:
+    #     json.dump(deagg_data, jsonfile)
+    np.save(deagg_filename,deagg_data)
+    log.info(f'saved deagg results to {deagg_filename}')
+
+        
