2f3fcfe10c83a9bcc431c2c4802ade4ff8d49715
chrisdicaprio <christopher.dicaprio@gmail.com>
2023-05-01 14:43:17 +1200

    create batch jobs from existing configs

--- toshi_hazard_post/util/batch.py
+++ toshi_hazard_post/util/batch.py
@@ -23,6 +23,7 @@ def get_ecs_job_config(
     job_queue="BasicFargate_Q",
     extra_env: List[BatchEnvironmentSetting] = None,
     use_compression=False,
+    quote_config_string=False,
 ) -> Dict[str, Any]:
     """Build a config for batch."""
     if "Fargate" in job_definition:
@@ -80,6 +81,15 @@ def get_ecs_job_config(
             30720,  # value = 4
         ]
 
+    if quote_config_string:
+        task_config_json_quoted = config
+    else:
+        if use_compression:
+            task_config_json_quoted = compress_config(json.dumps(config))
+        else:
+            task_config_json_quoted = urllib.parse.quote(json.dumps(config))
+
+
     config = {
         "jobName": job_name,
         "jobQueue": job_queue,
@@ -90,9 +100,7 @@ def get_ecs_job_config(
             "environment": [
                 {
                     "name": "TASK_CONFIG_JSON_QUOTED",
-                    "value": compress_config(json.dumps(config))
-                    if use_compression
-                    else urllib.parse.quote(json.dumps(config)),
+                    "value": task_config_json_quoted, 
                 },
                 {"name": "NZSHM22_TOSHI_S3_URL", "value": toshi_s3_url},
                 {"name": "NZSHM22_TOSHI_API_URL", "value": toshi_api_url},
