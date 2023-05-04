b0078c13b29b6cd525684f0674283d2fd85c1895
chrisdicaprio <christopher.dicaprio@gmail.com>
2023-05-04 14:58:32 +1200

    migrate toshi-hazard-haste grid calculation to THP. Run in bach on AWS

---
+++ toshi_hazard_post/hazard_grid/aws_gridded_hazard.py
@@ -0,0 +1,158 @@
+import itertools
+
+from dataclasses import asdict
+from typing import Iterable, Dict, Generator
+
+import boto3
+
+from nzshm_common.location import CodedLocation
+
+import toshi_hazard_post.hazard_grid.grid_task
+from toshi_hazard_post.hazard_grid.gridded_hazard import DistributedGridTaskArguments
+from toshi_hazard_post.util import BatchEnvironmentSetting, get_ecs_job_config
+from toshi_hazard_post.local_config import API_URL, S3_URL
+
+NUM_MACHINES = 300
+NUM_WORKERS = 4
+TIME_LIMIT = 10*60
+MEMORY = 15360
+
+def batch_job_config( task_arguments: Dict, job_arguments: Dict, task_id: int):
+
+    job_name = f"ToshiHazardPost-GriddedHazard-{task_id}"
+    config_data = dict(task_arguments=task_arguments, job_arguments=job_arguments)
+    
+    extra_env = [
+        BatchEnvironmentSetting(name="NZSHM22_HAZARD_STORE_STAGE", value="PROD"),
+        BatchEnvironmentSetting(name="NZSHM22_HAZARD_STORE_REGION", value="ap-southeast-2"),
+        BatchEnvironmentSetting(name="NZSHM22_HAZARD_POST_WORKERS", value=str(NUM_WORKERS)),
+    ]
+    return get_ecs_job_config(
+        job_name,
+        config_data,
+        toshi_api_url=API_URL,
+        toshi_s3_url=S3_URL,
+        task_module=toshi_hazard_post.hazard_grid.grid_task.__name__,
+        time_minutes=TIME_LIMIT,
+        memory=MEMORY,
+        vcpu=NUM_WORKERS,
+        job_definition="BigLeverOnDemandEC2-THP-HazardAggregation",
+        job_queue="ToshiHazardPost_HazAgg_JQ",  # "BigLever_32GB_8VCPU_v2_JQ", #"BigLeverOnDemandEC2-job-queue"
+        extra_env=extra_env,
+        use_compression=True,
+    )
+
+def tasks_by_chunk(
+    poe_levels: Iterable[float],
+    hazard_model_ids: Iterable[str],
+    vs30s: Iterable[float],
+    imts: Iterable[str],
+    aggs: Iterable[str],
+    chunk_size: int,
+) -> Generator[DistributedGridTaskArguments, None, None]:
+
+    count = 0
+    total = 0
+    task_chunk = DistributedGridTaskArguments(
+        location_grid_id='',
+        poe_levels=[],
+        hazard_model_ids=[],
+        vs30s=[],
+        imts = [],
+        aggs = [],
+        filter_locations = [],
+    )
+    for (poe, hazard_model_id, vs30, imt, agg) in itertools.product(
+        poe_levels, hazard_model_ids, vs30s, imts, aggs
+    ):
+        count += 1
+        total += 1
+        task_chunk.poe_levels.append(poe)
+        task_chunk.hazard_model_ids.append(hazard_model_id)
+        task_chunk.vs30s.append(vs30)
+        task_chunk.imts.append(imt)
+        task_chunk.aggs.append(agg)
+        if count == chunk_size:
+            yield task_chunk
+            count = 0
+            task_chunk = DistributedGridTaskArguments(
+                location_grid_id='',
+                poe_levels=[],
+                hazard_model_ids=[],
+                vs30s=[],
+                imts = [],
+                aggs = [],
+                filter_locations = [],
+            )
+        elif total == len(poe_levels) * len(hazard_model_ids) * len(vs30s) * len(imts) * len(aggs):
+            yield task_chunk
+
+
+def batch_job_configs(
+    location_grid_id: str,
+    poe_levels: Iterable[float],
+    hazard_model_ids: Iterable[str],
+    vs30s: Iterable[float],
+    imts: Iterable[str],
+    aggs: Iterable[str],
+    filter_locations: Iterable[CodedLocation] = None,
+):
+    
+    task_count = 0
+    items_processed = 0
+
+    for task_chunk in tasks_by_chunk(
+        poe_levels,
+        hazard_model_ids,
+        vs30s,
+        imts,
+        aggs,
+        chunk_size=NUM_WORKERS
+    ):
+
+        data = DistributedGridTaskArguments(
+            location_grid_id=location_grid_id,
+            poe_levels=task_chunk.poe_levels,
+            hazard_model_ids=task_chunk.hazard_model_ids,
+            vs30s=task_chunk.vs30s,
+            imts=task_chunk.imts,
+            aggs=task_chunk.aggs,
+            filter_locations=filter_locations,
+        )
+        items_processed += NUM_WORKERS
+        task_count += 1
+        yield batch_job_config(
+            task_arguments=asdict(data),
+            job_arguments=dict(task_id=task_count, num_machines=NUM_MACHINES),
+            task_id = task_count,
+        )
+
+
+def distribute_gridded_hazard(
+    location_grid_id: str,
+    poe_levels: Iterable[float],
+    hazard_model_ids: Iterable[str],
+    vs30s: Iterable[float],
+    imts: Iterable[str],
+    aggs: Iterable[str],
+    filter_locations: Iterable[CodedLocation] = None,
+):
+
+    batch_client = boto3.client(
+            service_name='batch', region_name='us-east-1', endpoint_url='https://batch.us-east-1.amazonaws.com'
+        )
+    
+    for job_config in batch_job_configs(
+        location_grid_id,
+        poe_levels,
+        hazard_model_ids,
+        vs30s,
+        imts,
+        aggs,
+        filter_locations,
+    ):
+        print('AWS_CONFIG: ', job_config)
+        print()
+        res = batch_client.submit_job(**job_config)
+        print(res)
+        print()
---
+++ toshi_hazard_post/hazard_grid/gridded_hazard.py
@@ -0,0 +1,170 @@
+# coding: utf-8
+
+import itertools
+import logging
+import multiprocessing
+from collections import namedtuple
+from dataclasses import dataclass
+from typing import Iterable, Dict, List
+
+import numpy as np
+from nzshm_common.grids import RegionGrid
+from nzshm_common.location import CodedLocation
+from toshi_hazard_store import model, query_v3
+from pynamodb.exceptions import PutError
+
+from .gridded_poe import compute_hazard_at_poe
+
+log = logging.getLogger(__name__)
+INVESTIGATION_TIME = 50
+SPOOF_SAVE = False
+COV_AGG_KEY = 'cov'
+
+GridHazTaskArgs = namedtuple("GridHazTaskArgs", "location_keys poe_levels location_grid_id hazard_model_id vs30 imt agg")
+
+@dataclass
+class DistributedGridTaskArguments:
+    location_grid_id: str
+    poe_levels: List[float]
+    hazard_model_ids: List[str]
+    vs30s: List[float]
+    imts: List[str]
+    aggs: List[str]
+    filter_locations: List[CodedLocation]
+
+def process_gridded_hazard(location_keys, poe_levels, location_grid_id, hazard_model_id, vs30, imt, agg):
+    grid_accel_levels: Dict[float, List] = {poe: [None for i in range(len(location_keys))] for poe in poe_levels}
+    for haz in query_v3.get_hazard_curves(location_keys, [vs30], [hazard_model_id], imts=[imt], aggs=[agg]):
+        accel_levels = [val.lvl for val in haz.values]
+        poe_values = [val.val for val in haz.values]
+        index = location_keys.index(haz.nloc_001)
+        for poe_lvl in poe_levels:
+            try:
+                grid_accel_levels[poe_lvl][index] = compute_hazard_at_poe(poe_lvl, accel_levels, poe_values, INVESTIGATION_TIME)
+            except ValueError as err:
+                log.warning(
+                    'Error in compute_hazard_at_poe: %s, poe_lvl %s, haz_mod %s, vs30 %s, imt %s, agg %s'
+                    % (err, poe_lvl, hazard_model_id, vs30, imt, agg)
+                )
+                continue
+            log.debug('replaced %s with %s' % (index, grid_accel_levels[poe_lvl][index]))
+
+    if agg == 'mean':
+        for poe_lvl in poe_levels:
+            grid_covs: List = [None for i in range(len(location_keys))]
+            for cov in query_v3.get_hazard_curves(location_keys, [vs30], [hazard_model_id], imts=[imt], aggs=[COV_AGG_KEY]):
+                # cov_accel_levels = [val.lvl for val in cov.values]
+                cov_values = [val.val for val in cov.values]
+                index = location_keys.index(cov.nloc_001)
+                grid_covs[index] = np.exp(
+                    np.interp(np.log(grid_accel_levels[poe_lvl][index]), np.log(accel_levels), np.log(cov_values))
+                )
+
+            yield model.GriddedHazard.new_model(
+                hazard_model_id=hazard_model_id,
+                location_grid_id=location_grid_id,
+                vs30=vs30,
+                imt=imt,
+                agg=COV_AGG_KEY,
+                poe=poe_lvl,
+                grid_poes=grid_covs,
+            )
+
+    for poe_lvl in poe_levels:
+        yield model.GriddedHazard.new_model(
+            hazard_model_id=hazard_model_id,
+            location_grid_id=location_grid_id,
+            vs30=vs30,
+            imt=imt,
+            agg=agg,
+            poe=poe_lvl,
+            grid_poes=grid_accel_levels[poe_lvl],
+        )
+
+
+class GriddedHAzardWorkerMP(multiprocessing.Process):
+    """A worker that batches and saves records to DynamoDB."""
+
+    def __init__(self, task_queue):
+        multiprocessing.Process.__init__(self)
+        self.task_queue = task_queue
+
+    def run(self):
+        log.info("worker %s running." % self.name)
+        proc_name = self.name
+
+        while True:
+            nt = self.task_queue.get()
+            if nt is None:
+                # Poison pill means shutdown
+                self.task_queue.task_done()
+                log.info('%s: Exiting' % proc_name)
+                break
+
+            # with model.GriddedHazard.batch_write() as batch:
+            for ghaz in process_gridded_hazard(*nt):
+                if SPOOF_SAVE is False:
+                    # batch.save(ghaz)
+                    try:
+                        ghaz.save()
+                        log.info('save %s' % ghaz)
+                    except PutError:
+                        log.warn('could not save %s, likely item already exists' % ghaz)
+
+            self.task_queue.task_done()
+            log.info('%s task done.' % self.name)
+
+
+def calc_gridded_hazard(
+    location_grid_id: str,
+    poe_levels: Iterable[float],
+    hazard_model_ids: Iterable[str],
+    vs30s: Iterable[float],
+    imts: Iterable[str],
+    aggs: Iterable[str],
+    num_workers: int,
+    filter_locations: Iterable[CodedLocation] = None,
+):
+
+    log.debug(
+        'calc_gridded_hazard( grid: %s poes: %s models: %s vs30s: %s imts: %s aggs: %s'
+        % (location_grid_id, poe_levels, hazard_model_ids, vs30s, imts, aggs)
+    )
+    count = 0
+    grid = RegionGrid[location_grid_id]
+
+    # print(grid.resolution)
+    locations = list(
+        map(lambda grd_loc: CodedLocation(grd_loc[0], grd_loc[1], resolution=grid.resolution), grid.load())
+    )
+
+    if filter_locations:
+        locations = list(set(locations).intersection(set(filter_locations)))
+
+    location_keys = [loc.resample(0.001).code for loc in locations]
+
+    log.debug('location_keys: %s' % location_keys)
+
+    task_queue: multiprocessing.JoinableQueue = multiprocessing.JoinableQueue()
+
+    log.debug('Creating %d workers' % num_workers)
+    workers = [GriddedHAzardWorkerMP(task_queue) for i in range(num_workers)]
+    for w in workers:
+        w.start()
+
+    for (hazard_model_id, vs30, imt, agg) in itertools.product(
+        hazard_model_ids, vs30s, imts, aggs
+    ):
+
+        t = GridHazTaskArgs(location_keys, poe_levels, location_grid_id, hazard_model_id, vs30, imt, agg)
+        task_queue.put(t)
+        count += 1
+
+    # Add a poison pill for each to signal we've done everything
+    for i in range(num_workers):
+        task_queue.put(None)
+
+    # Wait for all of the tasks to finish
+    task_queue.join()
+
+    log.info('calc_gridded_hazard() produced %s gridded_hazard rows ' % count)
