0a550685544b68ef8a9d7cead1bda2c7efb9dccd
chrisdicaprio <christopher.dicaprio@gmail.com>
2024-06-07 17:07:31 +1200

    increase test tolerance

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -3,12 +3,10 @@ from unittest import mock
 
 import numpy as np
 import pandas as pd
-import pytest
 
 import toshi_hazard_post.local_config
-
-# from toshi_hazard_post.aggregation import run_aggregation
-# from toshi_hazard_post.aggregation_args import AggregationArgs
+from toshi_hazard_post.aggregation import run_aggregation
+from toshi_hazard_post.aggregation_args import AggregationArgs
 
 fixture_dir = Path(__file__).parent / 'fixtures' / 'end_to_end'
 parquet_filepath = fixture_dir / 'rlz_probs.pq'
@@ -26,11 +24,11 @@ def test_end_to_end(load_mock, save_mock, monkeypatch):
 
     monkeypatch.setattr(toshi_hazard_post.local_config, 'get_config', mock_config)
     load_mock.return_value = pd.read_parquet(parquet_filepath)
-    df = pd.read_parquet(parquet_filepath)
-    assert df.loc[415, 'values'][0] == pytest.approx(0.060450900346040726)
-    assert aggs_expected[0][0] == pytest.approx(0.06003212930540347)
+    # df = pd.read_parquet(parquet_filepath)
+    # assert df.loc[415, 'values'][0] == pytest.approx(0.060450900346040726)
+    # assert aggs_expected[0][0] == pytest.approx(0.06003212930540347)
 
-    # agg_args = AggregationArgs(args_filepath)
-    # run_aggregation(agg_args)
-    # aggs = save_mock.mock_calls[0].args[0]
-    # assert np.allclose(aggs, aggs_expected, rtol=1e-8, atol=1e-12)
+    agg_args = AggregationArgs(args_filepath)
+    run_aggregation(agg_args)
+    aggs = save_mock.mock_calls[0].args[0]
+    assert np.allclose(aggs, aggs_expected)
